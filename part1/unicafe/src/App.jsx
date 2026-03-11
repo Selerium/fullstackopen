f766169348d5024ba2282d1e407b0e1ad44bea39
Selerium <johnadithya008@gmail.com>
2026-03-11 20:43:00 +0400

    ex1.10 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,20 +1,32 @@
 import { useState } from 'react'
 
+const Button = ({ name, onClick }) => {
+  return (
+    <button onClick={onClick}>{name}</button>
+  )
+}
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p>{text} {value}</p>
+  )
+}
+
 const Statistics = ({ good, neutral, bad, total }) => {
   return (
     <>
       <h1>statistics</h1>
       {total > 0 ?
         <>
-          <p>good {good}</p>
-          <p>neutral {neutral}</p>
-          <p>bad {bad}</p>
-          <p>all {total}</p>
-          <p>average {total > 0 ? (good - bad) / total : 0}</p>
-          <p>postive {total > 0 ? good / total : 0}%</p>
+          <StatisticLine text='good' value={good} />
+          <StatisticLine text='neutral' value={neutral} />
+          <StatisticLine text='bad' value={bad} />
+          <StatisticLine text='total' value={total} />
+          <StatisticLine text='average' value={total > 0 ? (good - bad) / total : 0} />
+          <StatisticLine text='positve' value={(total > 0 ? good / total : 0) + '%'} />
         </> :
         <p>No feedback given</p>
-    }
+      }
     </>
   )
 }
@@ -29,9 +41,9 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button onClick={() => setGood(good + 1)} name='good' />
+      <Button onClick={() => setNeutral(neutral + 1)} name='neutral' />
+      <Button onClick={() => setBad(bad + 1)} name='bad' />
       <Statistics good={good} bad={bad} neutral={neutral} total={total} />
     </div>
   )
