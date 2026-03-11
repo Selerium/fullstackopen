b95f7afbdcbc6fd2daf6205c2ecad22f6bc4294d
Selerium <johnadithya008@gmail.com>
2026-03-12 01:02:24 +0400

    ex2.11 completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,8 +2,15 @@ import { useState } from 'react'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then((response) => console.log(response))
+  }, [])
+
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
@@ -21,8 +28,8 @@ const App = () => {
       <h2>Phonebook</h2>
       <Filter searchFilter={searchFilter} setSearchFilter={setSearchFilter} />
       <h3>add a new</h3>
-      <PersonForm 
-        newName={newName} 
+      <PersonForm
+        newName={newName}
         setNewName={setNewName}
         newNumber={newNumber}
         setNewNumber={setNewNumber}
